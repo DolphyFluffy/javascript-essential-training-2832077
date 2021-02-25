071eb95284d635551f59a4e5aa5986859aba70e5
rachel.leung <rachel.leung@indexexchange.com>
2021-02-25 14:05:44 -0500

    Finished some practices

--- Practice/09_04/script.js
+++ Practice/09_04/script.js
@@ -5,3 +5,40 @@
  * - Add an event listener to each grid cell to change its background color when it is clicked.
  * - Add an event listener to a specific key on the keyboard to change the background color of the whole page - from dark to light and back again.
  */
+
+let gridCells = document.querySelectorAll(".cell");
+
+const randColor = () => {
+    let hexColor = Math.floor(Math.random() * 16777215).toString(16);
+    return hexColor;
+  };
+
+gridCells.forEach((cell) => {
+    cell.addEventListener("mouseenter", () => {
+        cell.style.outline = "2px solid" + `#${randColor()}`;
+    });
+
+    cell.addEventListener("mouseleave", () => {
+        cell.style.outline = "";
+    });
+    cell.addEventListener("click", () => {
+        if (cell.style.backgroundColor) {
+            cell.style.backgroundColor = "";
+        } else {
+            cell.style.backgroundColor = `#${randColor()}`;
+        }
+    });
+});
+
+const body = document.body;
+body.addEventListener("keydown", (event) => {
+  // event.code holds the current key pressed:
+  console.log(event.code);
+
+  // Test for KeyD (the "d" key)
+  if (event.code === "KeyD") {
+    body.style.backgroundColor === ""
+      ? (body.style.backgroundColor = "hsl(201, 34%, 13%)")
+      : (body.style.backgroundColor = "");
+  }
+});
